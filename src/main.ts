813ed3f5c701e039249d823fb88af2af345f8cfa
Mauricio T <mptorrejon@gmail.com>
2016-07-06 10:25:53 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /// <reference path="../typings/globals/es6-shim/index.d.ts" />
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './components/app.component';
+import { AppComponent } from './components/header.component';
 import { CONFIG } from './constants/config';
 
 bootstrap( AppComponent, [CONFIG] );
